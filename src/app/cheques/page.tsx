1ac1dfaa9681a8c3b165297914b14739de7f9539
MaximusL9 <Maximusmendezgonzalez@gmail.com>
2026-03-28 22:53:24 -0600

    Feat: Implementar paginas extras

---
+++ src/app/cheques/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+const PayHeader: React.FC = () => {
+  return (
+    <div className="mt-8 mb-8 flex items-center gap-4">
+      {/* Línea horizontal verde a la izquierda, igual que en la imagen */}
+      <div className="w-16 h-1 bg-[#006341] rounded-full"></div>
+
+      {/* Texto "Cheques" */}
+      <h2 className="text-3xl font-bold italic text-[#006341] tracking-wider uppercase">
+        Cheques
+      </h2>
+    </div>
+  );
+};
+
+export default PayHeader;
